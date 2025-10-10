0bce4b95e5bacd52c1b14f18f4a8ac8e0eea24ea
Ezz <abdelrahmanezzelregal26@gmail.com>
2025-10-10 16:51:30 +0300

    preparing for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,30 @@
-require("dotenv").config();
-const express = require("express");
-const cookieParser = require("cookie-parser");
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
 const app = express();
-const authRoutes = require("./routes/auth.route");
-const messageRoutes = require("./routes/message.route");
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+// Constants
 const port = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+// Make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from "express";
 const router = express.Router();
 
 router.get("/signup", (req, res) => {
@@ -13,4 +13,4 @@ router.get("/logout", (req, res) => {
   res.send("logout endpoint");
 });
 
-module.exports = router;
+export default router;
